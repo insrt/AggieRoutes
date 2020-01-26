3319b06ad4078c5c34b99db45a240f97ac959e00
insrt <sumodude529@gmail.com>
2020-01-26 08:06:44 -0600

    edited home page

--- AggieRoutes/pages/forms.py
+++ AggieRoutes/pages/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LocationForm(forms.Form):
-    origin = forms.CharField(label='origin', max_length=100)
-    dest = forms.CharField(label='dest', max_length=100)
+    origin = forms.CharField(label='Start Point', max_length=100)
+    dest = forms.CharField(label='End Point', max_length=100)
